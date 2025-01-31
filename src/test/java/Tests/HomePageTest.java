c63b1b41e5e7680ed8bf4ed26a254d8caaf8c020
AbdelRhmanMohamedNaser <62797153+AbdelRhmanMohamedNaser@users.noreply.github.com>
2025-01-31 18:34:18 +0200

    Update HomePageTest.java

--- src/test/java/Tests/HomePageTest.java
+++ src/test/java/Tests/HomePageTest.java
@@ -7,21 +7,5 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class HomePageTest extends Testpage{
-    HomePage home;
-    PageBase page;
-    LoginPage login;
-    SoftAssert softAssert;
 
-
-    @Test
-    public void LoginUserWithCorrectEmailAndPassword() throws InterruptedException {
-        home = new HomePage(driver);
-        page =new PageBase(driver);
-        login =new LoginPage(driver);
-        softAssert = new SoftAssert();
-        Thread.sleep(3000);
-        page.clickOn(login.later);
-        String actResult=page.getText(login.dashboard);
-        softAssert.assertEquals(actResult,"Dashboard");
-    }
 }
